c37a83dda3ddf5c5ad2692739a8e043e496ae915
MUHAMMAD SARIM <muhammad.sarim001100@gmail.com>
2025-01-03 23:55:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//  take  a  prompt from user
+let word = prompt("Enter A  word")
+
+// now break words using split then
+//  reverse words using reverse method ,
+//   and then join words using oin method 
+
+let reversedWord = word.split("").reverse().join("")
+
+// now check reverse word 
+
+console.log(reversedWord)
